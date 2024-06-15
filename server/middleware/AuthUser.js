6d22f30aa20badf31fda4725487bb86f8f72aee4
jovianalthaf <mpialthaf@yahoo.co.id>
2024-06-15 16:19:30 +0700

    change algoritma JWT TOKEN from HS256 to RS256

--- server/middleware/AuthUser.js
+++ server/middleware/AuthUser.js
@@ -4,9 +4,22 @@ import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+const privateKey = process.env.JWT_PRIVATE.replace(/\\n/g, '/n');
+const publicKey = process.env.JWT_PUBLIC.replace(/\\n/g, '\n');
+
 // generate token jwt
 export const generateToken = (user) => {
-    return jwt.sign({ id: user.id, name: user.name, role: user.role, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const payload = {
+        id: user.id,
+        name: user.name,
+        role: user.role,
+        email: user.email,
+    };
+    //  MENGGUNAKAN ALGORITMA HS256
+    // return jwt.sign({ id: user.id, name: user.name, role: user.role, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+    // MENGGUNAKAN ALGORITMA RS256
+    return jwt.sign(payload, privateKey, { algorithm: 'RS256', expiresIn: '1h' });
 };
 
 export const authToken = (req, res, next) => {
@@ -17,7 +30,9 @@ export const authToken = (req, res, next) => {
     if (!token) return res.status(401).json({ msg: "Access denied, no token provided" });
 
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const verified = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+        //VERIFY DIBAWAH UNTUK ALGORITMA HS256
+        // const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
         next();
     } catch (error) {
